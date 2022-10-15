2f5a0543523ada6c4e61ac55d98ab7e05bf2a704
Vladislav Zavertaylo <102727549+vladislavzaver91@users.noreply.github.com>
2022-10-15 13:21:52 +0300

    Update index.js

--- src/PixabayAPI.js
+++ src/PixabayAPI.js
@@ -1,16 +1,27 @@
 export class PixabayAPI {
     #page = 1;
+    #perPage = 10;
     #query = '';
     #totalHits = 0;
 
-    getPhotos() {
-        const url = `https://pixabay.com/api/?key=30545528-143ee6a3dc060094e4755846c&q=${this.#query}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.#page}&per_page=40`;
-        return fetch(url).then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        })
+    #baseURL = 'https://pixabay.com/api/?key=30545528-143ee6a3dc060094e4755846c';
+    #paramURL = 'image_type=photo&orientation=horizontal&safesearch=true';
+
+    getPhotos = async () => {
+        const url = `${this.#baseURL}&q=${this.#query}&${this.#paramURL}&page=${this.#page}&per_page=${this.#perPage}`;
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+        
+
+        // return fetch(url).then(response => {
+        //     if (!response.ok) {
+        //         throw new Error(response.status);
+        //     }
+        //     return response.json();
+        // })
     }
 
 
@@ -27,8 +38,13 @@ export class PixabayAPI {
         this.#page += 1;
     }
 
+    resetPage() {
+        this.#page = 1;
+    }
+
+
     calculateTotalHits(total) {
-        this.#totalHits = Math.ceil(total / 40);
+        this.#totalHits = Math.ceil(total / this.#perPage);
     }
 
     get isShowLoadMore() {
--- src/index.js
+++ src/index.js
@@ -25,22 +25,75 @@ function handleSubmit (ev) {
     }
     pixabay.query = query;
 
-    pixabay.getPhotos(query).then(({ hits, total }) => {
-        renderPhotoGalleryMarkup(hits);
-        pixabay.calculateTotalHits(total);
+    clearPage();
+    initPade();
 
-        if (pixabay.isShowLoadMore) {
-            refs.loadMoreBtn.classList.remove('is-hidden');
+    async function initPade() {
+        try {
+            const data = await pixabay.getPhotos(query);
+            console.log(data);
+            // if (hits.length === 0) {
+            //     Notify.info('Sorry, there are no images matching your search query. Please try again.');
+            //     return;
+            // }
+            
+            // renderPhotoGalleryMarkup(hits);
+            
+            // pixabay.calculateTotalHits(total);
+            // Notify.success(`Hooray! We found ${total} images.`);
+    
+            // if (pixabay.isShowLoadMore) {
+            //     refs.loadMoreBtn.classList.remove('is-hidden');
+            // }
+        } catch (error) {
+            Notify.failure(error.message, 'Oops, something went wrong!');
+        clearPage();
         }
-    });
+        
+    }
+
+    pixabay.getPhotos(query).then(({ hits, total }) => {
+        console.log({ hits, total });
+
+    //     if (hits.length === 0) {
+    //         Notify.info('Sorry, there are no images matching your search query. Please try again.');
+    //         return;
+    //     }
+        
+    //     renderPhotoGalleryMarkup(hits);
+        
+    //     pixabay.calculateTotalHits(total);
+    //     Notify.success(`Hooray! We found ${total} images.`);
+
+    //     if (pixabay.isShowLoadMore) {
+    //         refs.loadMoreBtn.classList.remove('is-hidden');
+    //     }
+    // }).catch(err => {
+    //     Notify.failure(err.message, 'Oops, something went wrong!');
+    //     clearPage();
+    // });
 };
 
 function onLoadMoreClick() {
     pixabay.incrementPage();
 
+    if (!pixabay.isShowLoadMore) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+        Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+
     pixabay.getPhotos().then(({ hits }) => {
         renderPhotoGalleryMarkup(hits);
+    }).catch(err => {
+        Notify.failure(err.message, 'Oops, something went wrong!');
+        clearPage();
     });
+}
+
+function clearPage() {
+    pixabay.resetPage();
+    refs.galleryWrapper.innerHTML = '';
+    refs.loadMoreBtn.classList.add('is-hidden');
 }
 
 
